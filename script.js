6cc3f07333b75268893881445fe237bb08747d92
Rajendra Shrestha <rajendrashr@gmail.com>
2020-08-30 23:34:54 -0700

    Minor corrections

--- script.js
+++ script.js
@@ -1,94 +1,95 @@
 console.log("Hello all");
 // Event Listener for button
-$("button").on("click", function (event) {
-        event.preventDefault();
-        var APIKey = "b7224a3320e1c0e1ae22fd2c9b341469";
-        var search = $("#search").val();
-        // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + APIKey;
-        var uvindex = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat={lat}&lon={lon}";
-        console.log(uvindex);
-        console.log(search);
-
-        // ajax call to OpenWeatherMap
-        $.ajax({
-                url: queryURL, uvindex,
-                method: "GET"
-                //Store all of the retrieved data inside of an object called "response"
-        }).then(function (response) {
-                // Logging the queryURL
-                console.log(queryURL);
-                // Logging the queryURL
+$(document).ready(function () {
+        $("button").on("click", function (event) {
+                event.preventDefault();
+                var APIKey = "b7224a3320e1c0e1ae22fd2c9b341469";
+                var search = $("#search").val();
+                // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+                var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + APIKey;
+                var uvindex = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat={lat}&lon={lon}";
                 console.log(uvindex);
-                // Logging the resultin response
-                console.log(response);
+                console.log(search);
 
-                // Transferring content to HTML for current day
+                // ajax call to OpenWeatherMap
+                $.ajax({
+                        url: queryURL, uvindex,
+                        method: "GET"
+                        //Store all of the retrieved data inside of an object called "response"
+                }).then(function (response) {
+                        // Logging the queryURL
+                        console.log(queryURL);
+                        // Logging the queryURL
+                        console.log(uvindex);
+                        // Logging the resultin response
+                        console.log(response);
 
-                $(".city").html("<h3>" + response.city.name + "</h3>");
-                $(".date").html("Date:" + response.list[0].dt_txt);
-                $(".wind").text("Wind Speed:" + response.list[0].wind.speed);
-                $(".humidity").text("Humidity:" + response.list[0].main.humidity);
-                $(".temperature").text("Temperature(K):" + response.list[0].main.temp);
-                $(".uvindex").text("uvIndex:" + response.list[0].uvindex);
+                        // Transferring content to HTML for current day
 
-                // Transferring content to HTML for current day1
+                        $(".city").html("<h3>" + response.city.name + "</h3>");
+                        $(".date").html("Date:" + response.list[0].dt_txt);
+                        $(".wind").text("Wind Speed:" + response.list[0].wind.speed);
+                        $(".humidity").text("Humidity:" + response.list[0].main.humidity);
+                        $(".temperature").text("Temperature(K):" + response.list[0].main.temp);
+                        $(".uvindex").text("uvIndex:" + response.list[0].uvindex);
 
-                $(".city1").html("<h1>" + response.city.name + "</h1>");
-                $(".date1").html("Date:" + response.list[1].dt_txt);
-                // $(".wind1").text("Wind Speed:" + response.list[1].wind.speed);
-                $(".humidity1").text("Humidity:" + response.list[1].main.humidity);
-                $(".temp1").text("Temperature(K):" + response.list[1].main.temp);
+                        // Transferring content to HTML for current day1
 
-                // Transferring content to HTML for current day2
+                        $(".city1").html("<h1>" + response.city.name + "</h1>");
+                        $(".date1").html("Date:" + response.list[1].dt_txt);
+                        // $(".wind1").text("Wind Speed:" + response.list[1].wind.speed);
+                        $(".humidity1").text("Humidity:" + response.list[1].main.humidity);
+                        $(".temp1").text("Temperature(K):" + response.list[1].main.temp);
 
-                $(".city2").html("<h1>" + response.city.name + "</h1>");
-                $(".date2").html("Date:" + response.list[2].dt_txt);
-                // $(".wind2").text("Wind Speed:" + response.list[2].wind.speed);
-                $(".humidity2").text("Humidity:" + response.list[2].main.humidity);
-                $(".temp2").text("Temperature(K):" + response.list[2].main.temp);
+                        // Transferring content to HTML for current day2
 
-                // Transferring content to HTML for current day3
+                        $(".city2").html("<h1>" + response.city.name + "</h1>");
+                        $(".date2").html("Date:" + response.list[2].dt_txt);
+                        // $(".wind2").text("Wind Speed:" + response.list[2].wind.speed);
+                        $(".humidity2").text("Humidity:" + response.list[2].main.humidity);
+                        $(".temp2").text("Temperature(K):" + response.list[2].main.temp);
 
-                $(".city3").html("<h1>" + response.city.name + "</h1>");
-                $(".date3").html("Date:" + response.list[3].dt_txt);
-                // $(".wind3").text("Wind Speed:" + response.list[3].wind.speed);
-                $(".humidity3").text("Humidity:" + response.list[3].main.humidity);
-                $(".temp3").text("Temperature(K):" + response.list[3].main.temp);
+                        // Transferring content to HTML for current day3
 
-                // Transferring content to HTML for current day4
+                        $(".city3").html("<h1>" + response.city.name + "</h1>");
+                        $(".date3").html("Date:" + response.list[3].dt_txt);
+                        // $(".wind3").text("Wind Speed:" + response.list[3].wind.speed);
+                        $(".humidity3").text("Humidity:" + response.list[3].main.humidity);
+                        $(".temp3").text("Temperature(K):" + response.list[3].main.temp);
 
-                $(".city4").html("<h1>" + response.city.name + "</h1>");
-                $(".date4").html("Date:" + response.list[4].dt_txt);
-                // $(".wind4").text("Wind Speed:" + response.list[4].wind.speed);
-                $(".humidity4").text("Humidity:" + response.list[4].main.humidity);
-                $(".temp4").text("Temperature(K):" + response.list[4].main.temp);
+                        // Transferring content to HTML for current day4
 
-                // Transferring content to HTML for current day5
+                        $(".city4").html("<h1>" + response.city.name + "</h1>");
+                        $(".date4").html("Date:" + response.list[4].dt_txt);
+                        // $(".wind4").text("Wind Speed:" + response.list[4].wind.speed);
+                        $(".humidity4").text("Humidity:" + response.list[4].main.humidity);
+                        $(".temp4").text("Temperature(K):" + response.list[4].main.temp);
 
-                $(".city5").html("<h1>" + response.city.name + "</h1>");
-                $(".date5").html("Date:" + response.list[5].dt_txt);
-                // $(".wind5").text("Wind Speed:" + response.list[5].wind.speed);
-                $(".humidity5").text("Humidity:" + response.list[5].main.humidity);
-                $(".temp5").text("Temperature(K):" + response.list[5].main.temp);
+                        // Transferring content to HTML for current day5
 
-                console.log(response.list[4].main.humidity);
+                        $(".city5").html("<h1>" + response.city.name + "</h1>");
+                        $(".date5").html("Date:" + response.list[5].dt_txt);
+                        // $(".wind5").text("Wind Speed:" + response.list[5].wind.speed);
+                        $(".humidity5").text("Humidity:" + response.list[5].main.humidity);
+                        $(".temp5").text("Temperature(K):" + response.list[5].main.temp);
 
-                // Store last search history to local storage
+                        console.log(response.list[4].main.humidity);
 
-                function storeItem() {
-                        let lastSearch = JSON.stringify(search);
-                        localStorage.setItem("City", search);
+                        // Store last search history to local storage
 
-                        // Retrieve last search history from local storage
+                        function storeItem() {
+                                let lastSearch = JSON.stringify(search);
+                                localStorage.setItem("City", search);
 
-                        let storedSearch = localStorage.getItem("City");
-                        $(".search-item").text("Last search:  " + storedSearch);
-                        // return;
-                }
-                storeItem();
+                                // Retrieve last search history from local storage
+
+                                let storedSearch = localStorage.getItem("City");
+                                $(".search-item").text("Last search:  " + storedSearch);
+                                
+                                return;
+                        }
+                        storeItem();
+                });
         });
-
 });
-
 
